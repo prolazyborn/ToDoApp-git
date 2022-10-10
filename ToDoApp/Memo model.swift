1a9bf2993f7a7f097947b73321228d85ab9bb669
kim ji woong <125845@naver.com>
2022-10-10 16:29:19 +0900

    Memo
    Memo model

---
+++ ToDoApp/Memo model.swift
@@ -0,0 +1,15 @@
+//
+//  Memo model.swift
+//  ToDoApp
+//
+//  Created by Woong on 2022/10/10.
+//
+
+import Foundation
+
+
+struct Memo {
+    var memoText: String?
+    var date: Date?
+    var color: Int
+}
